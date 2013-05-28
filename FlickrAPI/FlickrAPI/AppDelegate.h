d52050c92be4e485d2705a8a7cb2222010d0efb2
Brian Lewis <brianelew@gmail.com>
2013-05-28 14:18:32 -0500

    initial commit

---
+++ FlickrAPI/FlickrAPI/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FlickrAPI
+//
+//  Created by Brian Lewis on 5/28/13.
+//  Copyright (c) 2013 Brian Lewis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FlickrAPI/FlickrAPI/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FlickrAPI
+//
+//  Created by Brian Lewis on 5/28/13.
+//  Copyright (c) 2013 Brian Lewis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
