04114ce7c85572ef6510467fc2db00cc8004df73
Brian Lewis <brianelew@gmail.com>
2013-05-28 16:39:31 -0500

    segmented control tags and location

--- FlickrAPI/FlickrAPI/ViewController.h
+++ FlickrAPI/FlickrAPI/ViewController.h
@@ -10,6 +10,5 @@
 
 @interface ViewController : UIViewController
 
-- (IBAction)nextPhoto:(id)sender;
 
 @end
--- FlickrAPI/FlickrAPI/ViewController.m
+++ FlickrAPI/FlickrAPI/ViewController.m
@@ -11,6 +11,8 @@
 @interface ViewController ()
 {
     __weak IBOutlet UIImageView *imageView;
+    __weak IBOutlet UIActivityIndicatorView *activityIndicator;
+    __weak IBOutlet UISegmentedControl *tagOrLocationControlOutlet;
         
     NSString *ApiKey;
     NSString *ApiSecret;
@@ -18,8 +20,12 @@
     
     int imageCounter;
 }
+- (IBAction)nextPhoto:(id)sender;
+- (IBAction)tagOrLocationControl:(id)sender;
 
 -(void)getPhotosFromFlickr;
+-(void)displayNextPhoto;
+
 @end
 
 @implementation ViewController
@@ -30,24 +36,46 @@
 
     ApiKey = @"4285143b45794604189ae812ab052343";
     ApiSecret = @"eeff2d90b373858a";
-    imageCounter = 0;
     
+    activityIndicator.color = [UIColor blueColor];
+
     [self getPhotosFromFlickr];
 }
 
 -(void)getPhotosFromFlickr
 {
+    imageCounter = 0;
+    
     //http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=4285143b45794604189ae812ab052343&tags=goblue
     
-    NSString *urlString = [NSString stringWithFormat:@"http://api.flickr.com/services/rest/?method=flickr.photos.search&format=json&nojsoncallback=1&api_key=%@&tags=goblue", ApiKey];
+    [activityIndicator startAnimating];
+    
+    NSString *urlString;
 
+    if (tagOrLocationControlOutlet.selectedSegmentIndex == 0) {
+    //search by tag
+    NSString *tags = @"goblue";
+    urlString = [NSString stringWithFormat:@"http://api.flickr.com/services/rest/?method=flickr.photos.search&format=json&nojsoncallback=1&api_key=%@&tags=%@", ApiKey, tags];
+    }
+    else {
+    /*search by location*/
+    CGFloat latitude = 28.418793; //Cinderella's Castle at Disney World's Magic Kingdom
+    CGFloat longitude = -81.581201;
+    
+    urlString = [NSString stringWithFormat:@"http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=%@&min_upload_date=20130201&lat=%f&lon=%f&radius=30&format=json&nojsoncallback=1", ApiKey, latitude, longitude];
+    }
+    
+    NSLog(@"%@", urlString);
     NSURL *url = [NSURL URLWithString:urlString];
     NSURLRequest *request = [NSURLRequest requestWithURL:url];
     [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue mainQueue] completionHandler:^(NSURLResponse *response, NSData *data, NSError *error) {
         NSDictionary *responseDictionary = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
         NSDictionary *photos = [responseDictionary objectForKey:@"photos"];
         photosArray = [photos objectForKey:@"photo"];
-                
+        
+        [activityIndicator stopAnimating];
+        activityIndicator.color = [UIColor whiteColor];
+        
         [self displayNextPhoto];
         
     }];
@@ -62,6 +90,10 @@
 - (IBAction)nextPhoto:(id)sender {
     imageCounter++;
     [self displayNextPhoto];
+}
+
+- (IBAction)tagOrLocationControl:(id)sender {
+    [self getPhotosFromFlickr];
 }
 
 -(void)displayNextPhoto
